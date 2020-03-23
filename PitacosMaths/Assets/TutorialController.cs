4ec1c83b4a121e4187f9f85201afcfdca427cb5f
juanFrancoSalcedo <62152724+juanFrancoSalcedo@users.noreply.github.com>
2020-03-23 16:57:05 -0500

    Switch Sprite Button Pause

---
+++ PitacosMaths/Assets/TutorialController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+
+    [SerializeField] private AnimationUIController shadow;
+   
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
