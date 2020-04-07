0ae53b9755b11d7fe9333772a7d03019d7ab27f2
juanFrancoSalcedo <62152724+juanFrancoSalcedo@users.noreply.github.com>
2020-04-07 16:16:10 -0500

    Cinema_1: particles leaf

--- PitacosMaths/Assets/Scripts/Tutorial/TutorialAssistant.cs
+++ PitacosMaths/Assets/Scripts/Tutorial/TutorialAssistant.cs
@@ -91,7 +91,7 @@ public class TutorialAssistant : MonoBehaviour
     {
         if (misionCompleted)
         {
-          //  MisionFinished();
+           MisionFinished();
         }
         else
         {
