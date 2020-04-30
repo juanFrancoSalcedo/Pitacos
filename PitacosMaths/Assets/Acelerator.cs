61d2266dc6079915be606870c0882e8265fcd568
juanFrancoSalcedo <62152724+juanFrancoSalcedo@users.noreply.github.com>
2020-04-29 19:24:42 -0500

    AceleratorTime

---
+++ PitacosMaths/Assets/Acelerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acelerator : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Time.timeScale = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Time.timeScale = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Time.timeScale = 4;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            Time.timeScale = 8;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            Time.timeScale = 20;
+        }
+    }
+}
